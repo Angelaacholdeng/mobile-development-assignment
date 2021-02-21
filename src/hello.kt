0921791bc2bb71b3b1457856ba402482b13fe24c
Angelaacholdeng <dengacholangela@gmail.com>
2021-02-21 08:04:59 +0300

    first commit

---
+++ src/hello.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val name:String="Achol Deng"
+    var age:Byte=23
+    var weightKg :Float=61.6f
+    var  kenyancitizenship:Boolean=false
+    println(name)
+    println(age)
+    println(weightKg)
+    println(kenyancitizenship)
+
+
+
+
+
+
+}
